7ea9410410be073675dbb1fbe45fa7901437321b
Anthony Chedid <anthonyaachedid@gmail.com>
2020-11-18 21:31:52 +0200

    created react app

--- src/main/java/com/anthonychedid/project/repository/VisitedRepository.java
+++
@@ -1,7 +0,0 @@
-package com.anthonychedid.project.repository;
-
-import com.anthonychedid.project.model.Visited;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VisitedRepository extends JpaRepository<Visited, Long> {
-}
