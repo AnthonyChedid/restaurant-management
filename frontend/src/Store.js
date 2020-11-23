9f2371b8526c39dba4415f5bd3ae107e6aeee322
Anthony Chedid <anthonyaachedid@gmail.com>
2020-11-23 04:43:27 +0200

    small store modification

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -6,8 +6,13 @@ import rootReducer from './reducers/index';
 
 const sagaMiddleware=createSagaMiddleware();
 
-const store = compose(applyMiddleware(sagaMiddleware),window.devToolsExtension && window.devToolsExtension(),
-)(createStore)(rootReducer);
+var middleware =applyMiddleware(sagaMiddleware);
+
+var composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default createStore( rootReducer, {}, composeEnhancers(
+    middleware
+));
+
 
 sagaMiddleware.run(rootSaga);
-export default store;
