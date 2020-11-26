bd04e3997b4cbbbd6723e8dae283e1e62285f0fc
Anthony Chedid <anthonyaachedid@gmail.com>
2020-11-26 16:43:09 +0200

    updated queries

--- queries.sql
+++ queries.sql
@@ -7,10 +7,10 @@ id_restaurant, address, cost, image, phone, restaurant_name, restaurant_type) VA
  '987654321'::character varying, 'La Ratatouille'::character varying, 'Italian'::character varying),
 
 ('3'::bigint, 'New York'::character varying, '9'::bigint, './images/bobsBurgers.jpg'::character varying,
- '132547689'::character varying, 'Bob's Burgers'::character varying, 'American'::character varying),
+ '132547689'::character varying, 'Bob''s Burgers'::character varying, 'American'::character varying),
 
 ('4'::bigint, 'Switzerland'::character varying, '30'::bigint, './images/chocolateFactory.jpg'::character varying,
- '087892346'::character varying, 'Charly's Chocolate Factory'::character varying, 'American'::character varying),
+ '087892346'::character varying, 'Charly''s Chocolate Factory'::character varying, 'American'::character varying),
 
 ('5'::bigint, 'Simpsons'::character varying, '8'::bigint, './images/krustyBurger.jpg'::character varying,
  '023336789'::character varying, 'Krusty Burger'::character varying, 'American'::character varying),
@@ -25,12 +25,12 @@ id_restaurant, address, cost, image, phone, restaurant_name, restaurant_type) VA
  '789687963'::character varying, 'Pizza Planet'::character varying, 'Italian'::character varying),
 
 ('9'::bigint, 'Florida'::character varying, '5'::bigint, './images/moe.jpg'::character varying,
- '987023223'::character varying, 'Moe's'::character varying, 'American'::character varying),
+ '987023223'::character varying, 'Moe''s'::character varying, 'American'::character varying),
 
 ('10'::bigint, 'Cleveland'::character varying, '9'::bigint, './images/brokenStool.jpg'::character varying,
  '070890878'::character varying, 'The Broken Stool'::character varying, 'American'::character varying),
 
 ('11'::bigint, 'Futurama'::character varying, '18'::bigint, './images/elzar.jpg'::character varying,
- '096780986'::character varying, 'Elzar's'::character varying, 'Italian'::character varying);
+ '096780986'::character varying, 'Elzar''s'::character varying, 'Italian'::character varying);
 
 
